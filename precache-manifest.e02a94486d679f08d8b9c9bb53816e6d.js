4564478b8c554ebfec0c8bec06a954679c0940dc
SchedulerApp <dev.olinski@outlook.com>
2025-01-22 02:05:22 +0000

    Updates

---
+++ precache-manifest.e02a94486d679f08d8b9c9bb53816e6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f833b6d9e0f1519b3c792dd7801278",
+    "url": "/schedulerapp/index.html"
+  },
+  {
+    "revision": "4f0cd227e6b5983e1469",
+    "url": "/schedulerapp/static/css/2.17e61147.chunk.css"
+  },
+  {
+    "revision": "faf9ce6ea739e262cac2",
+    "url": "/schedulerapp/static/css/main.f78e183f.chunk.css"
+  },
+  {
+    "revision": "4f0cd227e6b5983e1469",
+    "url": "/schedulerapp/static/js/2.a06426b6.chunk.js"
+  },
+  {
+    "revision": "17addde56dada538fe1bc33d341e4ae4",
+    "url": "/schedulerapp/static/js/2.a06426b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf9ce6ea739e262cac2",
+    "url": "/schedulerapp/static/js/main.e18d89dc.chunk.js"
+  },
+  {
+    "revision": "c5d25c31af3d31d4f81e",
+    "url": "/schedulerapp/static/js/runtime-main.2b9faefe.js"
+  }
+]);
